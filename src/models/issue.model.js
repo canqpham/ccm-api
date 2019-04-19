079fe75528dc5295f34bef6073fc5e2479ef0dad
Canq Pham <phamhongcang.qng@gmail.com>
2019-04-19 21:47:35 +0700

    update

--- src/models/issue.model.js
+++ src/models/issue.model.js
@@ -16,7 +16,7 @@ const IssueSchema = new Schema(
     issueStatus: {type: ObjectId, ref: 'issueStatus'}, // to do, in progress, test, done
     sprint: {type: ObjectId, ref: 'sprints'},
     workflow: {type: ObjectId, ref: 'workflow'},
-    attach: {type: ObjectId, ref: 'attachs'},
+    attachs: [{type: String}], // Save url of file
     subTaskOfIssue: {type: ObjectId, ref: 'issues'}, // a subTask belong only issue
     label: {type: ObjectId, ref: 'labels'},
     resloved: {type: Date},
